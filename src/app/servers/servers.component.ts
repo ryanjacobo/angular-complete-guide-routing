9cdcd52cbc7adc86c2a1983f28acdb1edc49ade3
Ryan Jacobo <ryanjacobo@yahoo.com>
2022-11-03 08:16:59 -0400

    first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,27 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  public servers: { id: number; name: string; status: string }[] = [];
+
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+
+  // Using Relative Paths in Programmatic Navigation
+  onReload() {
+    this.router.navigate(['servers'], { relativeTo: this.route });
+  }
+}
